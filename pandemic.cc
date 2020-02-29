934e06b7f70367ed7eb044b5f600569c8d9108c6
Jeffery Parker <36011518+peffjarker@users.noreply.github.com>
2020-02-29 14:35:47 -0500

    Update pandemic.cc

--- pandemic.cc
+++ pandemic.cc
@@ -158,7 +158,6 @@ int main(int argc, char *argv[]) {
   ready.resize(DNA1.length());
   ready_p.resize(DNA1.length());
   for (int i = 0; i < DNA1.length(); ++i) {
-    cout << i;
     ready[i].resize(DNA2.length());
     ready_p[i].resize(DNA2.length());
     for (int j = 0; j < DNA2.length(); ++j) {
