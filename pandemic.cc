1d7174621e5bbbeb18ec8a8697538d6c5dcb998f
Jeffery Parker <36011518+peffjarker@users.noreply.github.com>
2020-03-02 19:40:54 -0500

    Update pandemic.cc

--- pandemic.cc
+++ pandemic.cc
@@ -19,6 +19,7 @@
 
 using namespace std;
 
+string LS;
 vector<vector<int>> LSQ;
 vector<vector<pair<int, int>>> from;
 vector<vector<future<int>>> ready;
@@ -55,68 +56,57 @@ string LS(string &DNA1, string &DNA2, int i, int n) {
   cout << "DNA1 Length = " << DNA1.length() << endl;
   cout << "DNA2 Length = " << DNA2.length() << endl;
 
-
   int start = max(1, (i * (int)DNA2.length() / n));
   int end = min(((i + 1) * DNA2.length()) / n, DNA2.length());
 
-
-  #pragma omp parallel
-  {
   for (int l = 1; l <= DNA1.length(); l++) {
-    #pragma omp parallel for schedule (static, 1)
-    for (int j = 1; j <= DNA2.length(); j++) {
+    for (int j = start; j <= end; j++) {
       if (j == 1) {
-        ready[l-1][j].wait();
-        ready[l][j-1].wait();
+        ready[l - 1][j].wait();
+        ready[l][j - 1].wait();
       }
       if (DNA1[l - 1] == DNA2[j - 1]) {
         if (LSQ[l - 1][j - 1] + 1 > max(LSQ[l - 1][j], LSQ[l][j - 1])) {
-          ready_p[l][j].set_value(LSQ[l - 1][j - 1] + 1);
           LSQ[l][j] = LSQ[l - 1][j - 1] + 1;
           from[l][j] = make_pair(l - 1, j - 1);
         } else {
           if (LSQ[l - 1][j] > LSQ[l][j - 1]) {
-            ready_p[l][j].set_value(LSQ[l - 1][j]);
             LSQ[l][j] = LSQ[l - 1][j];
             from[l][j] = make_pair(l - 1, j);
           } else {
-            ready_p[l][j].set_value(LSQ[l][j-1]);
             LSQ[l][j] = LSQ[l][j - 1];
             from[l][j] = make_pair(l, j - 1);
           }
         }
       } else {
         if (LSQ[l - 1][j] > LSQ[l][j - 1]) {
-          ready_p[l][j].set_value(LSQ[l - 1][j]);
           LSQ[l][j] = LSQ[l - 1][j];
           from[l][j] = make_pair(l - 1, j);
         } else {
-          ready_p[l][j].set_value(LSQ[l][j-1]);
           LSQ[l][j] = LSQ[l][j - 1];
           from[l][j] = make_pair(l, j - 1);
         }
       }
     }
   }
-  }
-
-  string LS1;
-  int l1 = DNA1.length();
-  int l2 = DNA2.length();
-  while ((l1 != 0) && (l2 != 0)) {
-    pair<int, int> t;
-    t = from[l1][l2];
-    if ((t.first == l1 - 1) && (t.second == l2 - 1)) {
-      assert(DNA1[l1 - 1] == DNA2[l2 - 1]);
-      LS1.insert(0, 1, DNA1[l1 - 1]);
-    }
-    l1 = t.first;
-    l2 = t.second;
-  }
-  assert(LS1.length() == LSQ[DNA1.length()][DNA2.length()]);
-  return LS1;
 }
 
+string LS1;
+int l1 = DNA1.length();
+int l2 = DNA2.length();
+while ((l1 != 0) && (l2 != 0)) {
+  pair<int, int> t;
+  t = from[l1][l2];
+  if ((t.first == l1 - 1) && (t.second == l2 - 1)) {
+    assert(DNA1[l1 - 1] == DNA2[l2 - 1]);
+    LS1.insert(0, 1, DNA1[l1 - 1]);
+  }
+  l1 = t.first;
+  l2 = t.second;
+}
+
+LS += LS1;
+}
 
 int main(int argc, char *argv[]) {
   assert(argc == 3); // Fail if this isn't true.
@@ -151,24 +141,19 @@ int main(int argc, char *argv[]) {
     LSQ[0][i] = 0;
     from[0][i] = make_pair(-1, -1);
   }
-
-  ready.resize(DNA1.length());
-  ready_p.resize(DNA1.length());
   for (int i = 1; i < DNA1.length() + 1; i++) {
-    ready[i].resize(DNA2.length() + 1);
-    ready_p[i].resize(DNA2.length() + 1);
     LSQ[i][0] = 0;
     from[i][0] = make_pair(-1, -1);
-    for (int j = 2; j < DNA2.length() + 1; ++j) {
-      ready[i][j] = ready_p[i][j].get_future();
-    }
   }
 
-  string LS1 = LS(DNA1, DNA2, 0, 1);
+  thread t1(DNA1, DNA2, 0, 4);
+  thread t2(DNA1, DNA2, 1, 4);
+  thread t3(DNA1, DNA2, 2, 4);
+  thread t4(DNA1, DNA2, 3, 4);
 
   cout << LS1 << endl;
-  cout << "Similarity score 1 vs 2="
-       << LS1.length() / (DNA1.length() * 1.0) << endl;
-  cout << "Similarity score 2 vs 1="
-       << LS1.length() / (DNA2.length() * 1.0) << endl;
+  cout << "Similarity score 1 vs 2=" << LS1.length() / (DNA1.length() * 1.0)
+       << endl;
+  cout << "Similarity score 2 vs 1=" << LS1.length() / (DNA2.length() * 1.0)
+       << endl;
 }
